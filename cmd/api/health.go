6e198e335b587bf8c9df9b28afa61fe3b61d11c3
Aman Kumar <amankumar87057@gmail.com>
2025-03-04 23:01:31 +0530

    added json response structure

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,8 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) healthCheckerHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("OK"))
+	data := map[string]string{
+		"status":  "working",
+		"version": "0.0.1",
+	}
+
+	err := writeJson(w, http.StatusAccepted, data)
+
+	if err != nil {
+		writeJsonError(w, http.StatusInternalServerError, err.Error())
+	}
 }
